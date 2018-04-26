8ff6b766224829d863d90e4d1f457a5659a8bef4
Luis Brito <luiseduardo14@gmail.com>
2018-04-25 23:23:32 -0300

    Docs: Fix title

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,4 +2,4 @@
 
 export DOCS_VERSION="$(echo "console.log(require(\"./package.json\").version)" | node)";
 echo "Generating documentation for v$DOCS_VERSION...";
-./node_modules/.bin/typedoc --name "ts-framework (v$DOCS_VERSION)" --mode file --out ./docs --theme node_modules/devnup-typedoc-theme/bin lib/index.ts;
+./node_modules/.bin/typedoc --name "ts-framework-notification (v$DOCS_VERSION)" --mode file --out ./docs --theme node_modules/devnup-typedoc-theme/bin lib/index.ts;
